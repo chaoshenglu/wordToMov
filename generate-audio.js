caec9d97a80a5101f23726639e0de903095678c0
lixiang <271896700@qq.com>
2025-06-07 17:23:42 +0800

    1

---
+++ generate-audio.js
@@ -0,0 +1,7 @@
+let en_list = [
+    "She wore a thermal sweater to stay warm in the freezing weather.",
+    "He filled his thermos with hot coffee before going hiking.",
+    "Ice melting is an endothermic process because it absorbs heat from the surroundings.",
+    "Burning wood is an exothermic reaction because it releases heat.",
+    "The doctor used a thermometer to check if the child had a fever."
+];

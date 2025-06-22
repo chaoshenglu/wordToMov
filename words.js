d0c49067c8d133ff5725345d8cdbd2314b7c090a
lixiang <271896700@qq.com>
2025-06-22 20:30:06 +0800

    1

--- words.js
+++ words.js
@@ -2,31 +2,31 @@ const wordsData = {
   "words": [
     {
       "word": "affect",
-      "meaning": "影响(动词，造成影响)",
+      "meaning": "影响(v.造成影响)",
       "sentence": "Lack of sleep can negatively affect your concentration.",
       "translation": "睡眠不足会对你的注意力产生负面影响。"
     },
     {
       "word": "effect",
-      "meaning": "效果(名词，产生的影响)",
+      "meaning": "效果(n.产生的影响)",
       "sentence": "The new law had a significant effect on reducing pollution.",
       "translation": "这项新法律对减少污染产生了显著效果。"
     },
     {
       "word": "perfect",
-      "meaning": "完美（名词，彻底的影响）",
+      "meaning": "完美（n.彻底的影响）",
       "sentence": "She practiced for months to perfect her piano performance.",
       "translation": "她练习了数月，以使自己的钢琴演奏趋于完美。"
     },
     {
       "word": "infect",
-      "meaning": "传染（动词，不良影响）",
+      "meaning": "传染（v.不良影响）",
       "sentence": "The virus can infect people through close contact.",
       "translation": "这种病毒可通过密切接触传染给人类。"
     },
     {
       "word": "disinfect",
-      "meaning": "消毒（动词，避免不良影响）",
+      "meaning": "消毒（v.避免不良影响）",
       "sentence": "Hospitals use special chemicals to disinfect surgical tools.",
       "translation": "医院使用特殊化学剂对外科工具进行消毒。"
     }
